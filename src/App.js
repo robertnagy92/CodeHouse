a01cec2028201f536f581b07ddbc5d6c632dd14a
nagyrobert92 <nagyrobert1992@yahoo.com>
2021-05-13 11:16:13 +0200

    login, signup, logout working, tested in the database

--- src/App.js
+++ src/App.js
@@ -1,15 +1,129 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MyNav from './components/MyNav'
+import { Route, Switch, withRouter } from 'react-router-dom'
+import axios from 'axios'
+import config from './config'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    user: null,
+    error: null,
+    fetchingUser: true
+  }
+
+  handleSignUp = (e) => {
+    e.preventDefault()
+    const {username, email , password} = e.target
+    let newUser = {
+      username: username.value, 
+      email: email.value, 
+      password: password.value
+    }
+    
+    axios.post(`${config.API_URL}/api/signup`, newUser, {withCredentials: true})
+      .then((response) => {
+        //the real data is always in response.data
+          this.setState({
+            user: response.data //save the user info in the state
+          }, () => {
+              //Redirect after the user info has been fetched
+              this.props.history.push('/')
+          })
+      })
+      .catch(() => {
+        console.log('SignUp failed')
+      })
+  }
+
+  handleSignIn = async (e) => {
+    e.preventDefault()
+    const { email , password} = e.target
+    let newUser = {
+      email: email.value, 
+      password: password.value
+    }
+
+    axios.post(`${config.API_URL}/api/signin`, newUser, {withCredentials: true})
+      .then((response) => {
+        this.setState({
+          user: response.data,
+          error: null
+        }, () => {
+          this.props.history.push('/')
+        })
+      })
+      .catch((errorObj) => {
+        this.setState({
+          error: errorObj.response.data
+        })
+      })
+  }
+
+
+  handleLogout = () => {
+      axios.post(`${config.API_URL}/api/logout`, {}, {withCredentials: true})
+        .then(() => {
+          this.setState({
+            user: null
+          })
+        })
+        .catch((errorObj) => {
+          // the real error json is always is the .response.data 
+          this.setState({
+            error: errorObj.response.data
+          })
+      })
+  }
+
+
+
+    // Make sure all the initial data that you show to the user is fetched here
+  
+  componentDidMount(){
+  
+      axios.get(`${config.API_URL}/api/user`, {withCredentials: true}) 
+        .then((response) => {
+          this.setState({ 
+            user: response.data,
+            fetchingUser: false,
+          })
+        })
+        .catch((errorObj) => {
+          this.setState({
+            error: errorObj.response.data,
+            fetchingUser: false,
+          })
+        })
+  }
+
+  
+  render() {
+    const {error, user, fetchingUser} = this.state
+
+    // 
+    if(fetchingUser){
+      return <p>Loading . . . </p>
+    }
+
+    return (
+      <div>
+        <MyNav onLogout={this.handleLogout} user={user} />
+        <h1>Code House</h1>
+        <Switch>
+            <Route  path="/signin"  render={(routeProps) => {
+              return  <SignIn error={error} onSignIn={this.handleSignIn}  {...routeProps}  />
+            }}/>
+            <Route  path="/signup"  render={(routeProps) => {
+              return  <SignUp onSubmit={this.handleSignUp} {...routeProps}  />
+            }}/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App)
---
+++ src/components/SignIn.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function SignIn(props){
+
+    const { onSignIn, error } = props
+    return (
+        <form onSubmit={onSignIn}>
+            <div className="form-group">
+                <label htmlFor="InputEmail">Email address</label>
+                <input type="email" className="form-control" id="InputEmail" name="email" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="InputPassword">Password</label>
+                <input name="password" type="password" className="form-control" id="InputPassword" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            {
+                error && (<p style={{color:'red'}}>{error.error}</p>)
+            }
+        </form>
+    )
+}
+
+export default SignIn
